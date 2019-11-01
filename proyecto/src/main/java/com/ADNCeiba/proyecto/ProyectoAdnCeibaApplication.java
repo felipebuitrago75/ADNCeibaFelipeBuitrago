1e14d6ece763007e549a861cd7b1f4cd88f01021
Felipe Buitrago <felipebuitrago75@gmail.com>
2019-11-01 14:12:07 -0500

    [ADNCEIBA]Se establece esqueleto del proyecto

---
+++ proyecto/src/main/java/com/ADNCeiba/proyecto/ProyectoAdnCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.ADNCeiba.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAdnCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAdnCeibaApplication.class, args);
+	}
+
+}
---
+++ proyecto/src/test/java/com/ADNCeiba/proyecto/ProyectoAdnCeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ADNCeiba.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAdnCeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
